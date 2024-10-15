7183bbc222bfa9869c56928c706857255618af60
Artsiom Badun <abadun@amazon.com>
2024-10-15 11:18:35 -0700

    Initial commit

---
+++ test/nextor-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NextorCdk from '../lib/nextor-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/nextor-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NextorCdk.NextorCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
